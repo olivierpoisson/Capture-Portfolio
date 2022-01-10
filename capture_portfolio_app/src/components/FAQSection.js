bfb23c9d00ce6ae57bae250d23f500f455cafcad
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-01-10 17:54:11 -0500

    FAQ Section html added

---
+++ capture_portfolio_app/src/components/FAQSection.js
@@ -0,0 +1,41 @@
+
+
+const FAQSection = () => {
+    return (
+        <div className="faq">
+            <h2>
+                Any Questions <span>FAQ</span>
+            </h2>
+            <div className="question">
+                <h4>How do i start ?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, in.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, in.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, in.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What services do you offer ?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, in.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQSection
--- capture_portfolio_app/src/pages/AboutUs.js
+++ capture_portfolio_app/src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 //! Page Components
 import AboutSection from "../components/AboutSection"
 import ServicesSection from "../components/ServicesSection"
+import FAQSection from "../components/FAQSection"
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection/>
             <ServicesSection/>
+            <FAQSection/>
         </>
     )
 }
