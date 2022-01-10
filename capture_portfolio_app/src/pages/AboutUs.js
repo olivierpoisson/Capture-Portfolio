3ae5f5e3a31a9f5f1324846d2424f908caeb970f
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-01-10 17:47:18 -0500

    Services Section html

--- capture_portfolio_app/src/pages/AboutUs.js
+++ capture_portfolio_app/src/pages/AboutUs.js
@@ -1,9 +1,13 @@
 //! Page Components
 import AboutSection from "../components/AboutSection"
+import ServicesSection from "../components/ServicesSection"
 
 const AboutUs = () => {
     return (
-        <AboutSection/>
+        <>
+            <AboutSection/>
+            <ServicesSection/>
+        </>
     )
 }
 
