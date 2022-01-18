2f7114b103f15b14afe444cede6942618fa80885
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-01-18 00:31:51 -0500

    Contact page animation

--- capture_portfolio_app/src/pages/ContactUs.js
+++ capture_portfolio_app/src/pages/ContactUs.js
@@ -1,18 +1,74 @@
 import { motion } from "framer-motion";
-import {pageAnimation} from "../animation";
+import { pageAnimation, titleAnimation } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
     return (
-        <motion.div 
-            style={{background: "#fff"}} 
-            exit="exit" 
-            variants={pageAnimation} 
-            initial="hidden" 
-            animate="show"
-        >
-            <h1>Contact Us.</h1>
-        </motion.div>
-    )
-}
+        <ContactStyle
+            style={{ background: "#fff" }}
+            exit="exit"
+            variants={pageAnimation}
+            initial="hidden"
+            animate="show">
+            <Title>
+                <Hide>
+                    <motion.h2 variants={titleAnimation}>
+                        Get in touch.
+                    </motion.h2>
+                </Hide>
+            </Title>
+            <div className="">
+                <Hide>
+                    <Social variants={titleAnimation}>
+                        <Circle />
+                        <h2>Send us a message</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants={titleAnimation}>
+                        <Circle />
+                        <h2>Send us a message</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants={titleAnimation}>
+                        <Circle />
+                        <h2>Send us a message</h2>
+                    </Social>
+                </Hide>
+            </div>
+        </ContactStyle>
+    );
+};
+
+const ContactStyle = styled(motion.div)`
+    padding: 3rem 10rem;
+    color: #353535;
+    min-height: 90vh;
+`;
+
+const Title = styled.div`
+    margin-bottom: 4rem;
+    color: black;
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+        margin: 2rem;
+    }
+`;
+
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background-color: #353535;
+`;
 
 export default ContactUs;
